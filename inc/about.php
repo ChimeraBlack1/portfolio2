1851b5435d46ab111c01922683b66f8867d2b6a7
ChimeraBlack1 <chimerablaque@hotmail.com>
2018-02-09 22:15:14 -0500

    changed about me to part bk, part dev

--- inc/about.php
+++ inc/about.php
@@ -2,17 +2,14 @@
 <div class="container">
     <section class="section p-t-3" id="about">
         <h2 class="section-heading custom-section-heading">
-            About me
+            Part Bookkeeper, Part Web Developer
         </h2>
         
         <div class="row">
-            <div class="col-lg-4"  data-toggle="modal" data-target="#photo">
+            <div class="col-lg-6 m-b-3"  data-toggle="modal" data-target="#photo">
                 <img src="img/op/bigbar.jpg" alt="photo" class="custom-fluid-img ">
             </div>
-            <div class="col-lg-6 col-lg-offset-2">
-                <p class="lead p-t-1">
-                    Part Bookkeeper, Part Web Developer
-                </p>
+            <div class="col-lg-6  section-description killMarginLeftRight">
                 <p>
                     <span>
                         Hi my name is Chris, and I am a hybrid bookkeeper and web developer.  I have achieved CGA Level 3, and have over 7 years of experience as a professional bookkeeper.  I have worked with clients ranging in size from small business, to billion-dollar corporate trusts. 
@@ -20,13 +17,13 @@
                 </p>
                 <p>
                     <span>
-                         I am self-taught when it comes to web development, and I've learned various languages like HTML5, CSS3, JavaScript, Java, C#, Python and PHP. Early on I learned how to use the bootstrap framework with jQuery,and actually used it to make this site.  Now I prefer to use React.js because of the performance of the virtual DOM, as well as the modular approach to building web components.
+                         I am self-taught when it comes to web development, and I've learned various languages like HTML5, CSS3, JavaScript, Java, C#, Python and PHP. Early on I learned how to use the bootstrap framework with jQuery,and actually used it to make this site.  These days I tend to use React.js because of the performance of the virtual DOM, as well as the modular approach to building web components.
                     </span>
                 </p>
 
                 <p>
                     <span>
-                    Part of how I taught myself to code was through a site called <a  href="https://teamtreehouse.com/christopherparke" target="_blank">Treehouse</a> -- one of the most popular platforms for learning to code. I am ranked in the <a href="https://teamtreehouse.com/christopherparke" target="_blank"> top 0.0025% (top 1,000 out of 400,000+) of web developers </a> on  Treehouse and continue learning to this day.
+                    Part of how I taught myself to code was through a site called Treehouse -- one of the most popular platforms for learning to code. I am <a href="https://teamtreehouse.com/christopherparke" target="_blank"> ranked in the  top 0.0025% (top 1,000 out of 400,000+) of web developers </a> on  Treehouse and continue learning to this day.
                     </span>
                 </p>
             </div>
