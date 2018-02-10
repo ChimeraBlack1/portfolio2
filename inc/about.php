f33e8427839875264c7f955639e63b518e31fa82
ChimeraBlack1 <chimerablaque@hotmail.com>
2018-02-09 21:24:06 -0500

    changed what text has the second link to treehouse

--- inc/about.php
+++ inc/about.php
@@ -20,9 +20,13 @@
                 </p>
                 <p>
                     <span>
-                         I am self-taught when it comes to web development, and I've learned various languages like HTML5, CSS3, JavaScript, Java, C#, Python and PHP. Early on I learned how to use the bootstrap framework with jQuery,and actually used it to make this site.  Now I prefer to use React.js because of the performance of the virtual DOM, as well as the modular approach to building web components.<br/> 
-                         
-                         Part of how I taught myself to code was through a site called <a  href="https://teamtreehouse.com/christopherparke" target="_blank">Treehouse</a> -- one of the most popular platforms for learning to code. I am ranked in the top 0.0025% (top 1,000 out of 400,000+) of web developers on <a  href="https://teamtreehouse.com/christopherparke" target="_blank">Treehouse</a> and continue learning to this day.
+                         I am self-taught when it comes to web development, and I've learned various languages like HTML5, CSS3, JavaScript, Java, C#, Python and PHP. Early on I learned how to use the bootstrap framework with jQuery,and actually used it to make this site.  Now I prefer to use React.js because of the performance of the virtual DOM, as well as the modular approach to building web components.
+                    </span>
+                </p>
+
+                <p>
+                    <span>
+                    Part of how I taught myself to code was through a site called <a  href="https://teamtreehouse.com/christopherparke" target="_blank">Treehouse</a> -- one of the most popular platforms for learning to code. I am ranked in the <a href="https://teamtreehouse.com/christopherparke" target="_blank"> top 0.0025% (top 1,000 out of 400,000+) of web developers </a> on  Treehouse and continue learning to this day.
                     </span>
                 </p>
             </div>
